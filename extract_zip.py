c0659812fe4a6805449d2e4ef48fee6612112676
b90303046 <b90303046@gmail.com>
2024-09-16 16:32:40 +0800

    modified:   extract_zip.py

--- extract_zip.py
+++ extract_zip.py
@@ -6,14 +6,16 @@ import sys
 import os 
 from pathlib import Path
 
-
 """
-輸入邏輯: 我要將D:\Housing 中的新(舊)檔案存到裡面的資料夾 sep_data中
-python extract_zip.py 'D:\Housing' 'sep_data' 1
+Simple direction:
+Command in powershell : 'python extract_zip.py  loc1 loc2  num'
+Extract the zip file in loc1 to loc2 with data type num
+num = 0 :  quarterly data
+num = 1 :  latest data (release every 10 days)
 """
 
 if len(sys.argv) != 4:
-    print("輸入變數不足, 離開程式...")
+    print("Not enough input, exit the program")
     sys.exit(1)
 
 
@@ -21,16 +23,16 @@ basic_loc = sys.argv[1]
 basic_loc_folder = sys.argv[2]
 basic_file_num = int(sys.argv[3])  # 可能為6 或 8
 filetype =[6,8]
-new_folder = basic_loc  + basic_loc_folder
+new_folder =  basic_loc_folder
 
 print(new_folder)
 
 try:
     os.mkdir(new_folder)
 except Exception as EEE:
-    print(EEE)
-    print(type(EEE).__name__)
-    print('資料夾已存在')
+    #print(EEE)
+    #print(type(EEE).__name__)
+    print('The directory has existed')
 
 
 zipfile_list = [ [jj[0:-4], jj] for jj in os.listdir(basic_loc) if re.search(r'\.zip',jj)]
@@ -38,8 +40,8 @@ zipfile_list = [jj for jj in zipfile_list if len(jj[0])== filetype[basic_file_nu
  
 
 for kk in zipfile_list:
-    extract_file = basic_loc+ '\\'+kk[1]
-    save_loc = new_folder + kk[0]
-    pprint(extract_file) 
-    with zipfile.ZipFile(extract_file,'r') as zzz:
-           zzz.extractall(save_loc)       
+     extract_file = basic_loc+ '\\'+kk[1]
+     save_loc = new_folder+'\\' + kk[0]
+     print(save_loc)
+     with zipfile.ZipFile(extract_file,'r') as zzz:
+             zzz.extractall(save_loc)       
