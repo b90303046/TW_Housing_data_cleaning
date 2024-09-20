b5531505251801bdf1b196389178cfea24a8954f
b90303046 <b90303046@gmail.com>
2024-09-20 10:12:08 +0800

    **Improved commit message:** **Subject:** Refactored RETR/collect_rawdata.py, updated documentation(README.md), and improved project configuration
    **RETR/collect_rawdata.py:**
        * Fixed bugs
        * Improved code readability and maintainability
    **README.md:**
        * Added more detailed explanations (extract_zip.py and RETR/collect_rawdata.py)
    **.gitignore:**
        * Added launch.json for debugging exclusion

--- RETR/collect_rawdata.py
+++ RETR/collect_rawdata.py
@@ -1,13 +1,16 @@
 #%%
-import pandas as pd 
+import pandas as pd
+import sys 
 import numpy as numpy
 import re
 from pathlib import Path
 import os
 import datetime as dt
 import pickle 
+from pprint import pprint
+from colorama import Fore, Style
 """
-函數說明
+Code Contents:
 1. city_code    : 列出各城市與代碼列表 
 2. list_loc     : 列出資料夾(Quarterly_Data or sep_data)中的所有實價登錄的"上傳期間"資料夾
 3. merge_csv    : 將"上傳期間"資料夾內符合條件的 csv檔案進行整併
@@ -17,78 +20,105 @@ import pickle
 7. load_pickle  : 給定位置與檔案名稱, 匯出整理的檔案
 """
 
+loc0 = Path(__file__).parent
 
 
-
-#file_saved_loc = Path(':\\Housing')  # 檔案存放位置
-file_saved_loc = Path('D:\\Housing')
-
-def city_code():
-    """
-    城市代碼, 確保位置內要有manifest.csv
-    """
-    man_loc  = file_saved_loc /'Data' /'manifest.csv'
-    manifest = pd.read_csv(man_loc, encoding='utf8')
-    pattern_sale ="(^.{3})" # take first three words
-    manifest.loc[:,'description'] = manifest['description'].\
-     str.extract(pattern_sale)
-    manifest['name'] = manifest['name'].apply(lambda x: x[0])
-    manifest = manifest.drop('schema', axis=1).drop_duplicates(ignore_index=True)
-    manifest = dict(zip(manifest['name'], manifest['description']))
-    return manifest
-
-
-def list_loc(fname):
-    """
-    列出資料夾中的所有實價登錄的"上傳期間"資料夾
-    """
-    parent_file = file_saved_loc /fname
-    all_loc = [ [jj, parent_file /jj] for jj in os.listdir(parent_file) if  not re.search('.csv',jj)]
-    return  all_loc
 
  
-def merge_csv(csv_key_id, loc0):
-    """
-    進入子資料夾loc0後, 讀取所有該資料夾內符合條件的的csv檔案
-    """
-    csv_file = [jj for  jj in os.listdir(loc0[1]) if re.search(csv_key_id,jj)]
-    DF = pd.DataFrame() 
-    if len(csv_file)==0:
-        state1 = '{loc}資料夾中沒資料, 跳過...'.format(loc=loc0[0])
-    else:
-        for cc in csv_file:
-            csv_loc = loc0[1] /cc  # csv位置
-            city= city_code()[cc[0]]
-            try:  # 檢查讀取csv檔是否有錯誤
-                df0 = pd.read_csv(csv_loc, index_col = None, 
-                                    encoding='utf8', low_memory=False).iloc[1:,]
-                new_col = [jj.replace('(','').replace(')','') for jj in df0.columns]
-                df0.columns = new_col
-                df0['城市'] = city
-                DF = pd.concat([DF, df0], join='outer', ignore_index=True)
-            except Exception as ee:
-                textt = '讀取資料檔{a}有誤,請檢察'.format(a = csv_loc)
-                print(textt)
-                print(type(ee).__name__)
-                print(ee)
-                continue
-        state1 = '進入{a}資料夾:共有{b}個csv檔案; {c}筆資料'.format(a=loc0[0], 
-                                                   b= len(csv_file), c=DF.shape[0])
-    print(state1)
-    return DF
+
+def city_code():
+     """
+     There are city code in manifest.csv, 
+     This function converts manifest to dict file
+     """
+     man_loc  = loc0 /'manifest.csv'
+     manifest = pd.read_csv(man_loc, encoding='utf8')
+     pattern_sale ="(^.{3})" # take first three words
+     manifest.loc[:,'description'] = manifest['description'].str.extract(pattern_sale)
+     manifest['name'] = manifest['name'].apply(lambda x: x[0])
+     manifest = manifest.drop('schema', axis=1).drop_duplicates(ignore_index=True)
+     manifest = dict(zip(manifest['name'], manifest['description']))
+     return manifest
 
 
-def collect_data(csv_key_id, fname):
+# List all directories in root1
+def get_subfolder(loc):
+    Subfolders_info = [ [Path(loc) /jj, jj] for jj in os.listdir(loc) if not os.path.isdir(jj)]
+    return Subfolders_info
+
+ 
+ 
+def merge_csv(csv_id, loc0):
+     """
+     Load all .csv files that in the element of Subfolders 
+     csv_key = ['land_a.csv','land_b.csv','land_c.csv']
+     0: _a.csv: 成屋 ; 1: _b.csv: 預售屋 ;  2: _c.csv
+     """
+     csv_key = ['land_a.csv','land_b.csv','land_c.csv']
+     csv_file = [jj for  jj in os.listdir(loc0) if re.search(csv_key[csv_id],jj)]
+     DF = pd.DataFrame() 
+
+     err_list = [] # add  
+     if len(csv_file)==0:
+            state1 = '{cwd}資料夾中沒資料, 跳過...\n'.format(cwd=loc0)
+            print(state1)
+     else:
+           for cc in csv_file:
+              csv_loc = loc0 /cc  # csv位置
+              city= city_code()[cc[0]]
+              try:  # 檢查讀取csv檔是否有錯誤
+                 df0 = pd.read_csv(csv_loc, index_col = None, 
+                                     encoding='utf8', low_memory=False).iloc[1:,]
+                 new_col = [jj.replace('(','').replace(')','') for jj in df0.columns]
+                 df0.columns = new_col
+                 df0['城市'] = city
+                 DF = pd.concat([DF, df0], join='outer', ignore_index=True)
+              except Exception as ee:
+                 textt = '讀取資料檔{a}有誤,請檢察'.format(a = csv_loc)
+                 #print(textt)
+                 #print(type(ee).__name__)
+                 error_loc =  str(loc0) +'\\'+ cc +':  Row '+ re.findall(r'line (\d+)',str(ee))[0]
+                 err_list.append(error_loc)
+                 print('\n')
+                 continue
+           state1 = '共有{b}個csv檔案; {c}筆資料\n'.format(b= len(csv_file), c=DF.shape[0])           
+           print(state1)
+     return DF, err_list
+ 
+
+
+
+def collect_data(csv_id, loc, start=0):
     """
-    1.合併所有資料夾內符合條件的csv檔案
+    1.合併所有資料夾root1 內符合條件的csv檔案
     2.完成合併後, 轉為Dict格式
-    """
-    Temp_Dict = {}
-    subfolders = list_loc(fname)
-    for ss in subfolders:     
-          DF0 = merge_csv(csv_key_id, ss)
-          Temp_Dict[ss[0]] = DF0
-    return Temp_Dict
+    """   
+    csv_id = int(csv_id) # convert to integer
+    csv_dict ={0:'成屋',1:'預售屋',2:'租賃'}
+    print('執行{data_type}的資料清理合併...\n'.format(data_type = csv_dict[csv_id]))
+    Total_DF = pd.DataFrame()
+    err_total_list = []
+    Subfolders_info = get_subfolder(loc)
+    items = Subfolders_info[start:]
+    if not isinstance(items,list):
+         items= [items]    
+    for ss in items: 
+           print('進入{a}資料夾...'.format(a=ss[0]))  
+           DF0, err_list  = merge_csv(csv_id, ss[0])
+           DF0['data'] = ss[1]
+           Total_DF = pd.concat([Total_DF, DF0], join='outer', axis=0, ignore_index=True)
+           err_total_list.extend(err_list)
+
+    if len(err_total_list) !=0:
+         print(Style.BRIGHT +
+                Fore.YELLOW +'\n There is something wrong in zip raw file(s):\n'+ 
+                Style.RESET_ALL)
+         pprint(err_total_list)
+    return Total_DF, err_total_list
+
+#print(Subfolders_info[10])
+#merge_csv(1, Subfolders_info[10])
+
 
 
 def select_col(file_key=0):
@@ -112,7 +142,7 @@ def select_col(file_key=0):
     return col
 
 
-def save_file(DF_file, foldername,file_key, folder_key,file_name=""):
+def save_file(DF_file, foldername,file_key, folder_key):
     """
     將Dataframe 在foldername資料夾存成 pickle檔案, 命名為...
     foldername 為str
@@ -127,30 +157,56 @@ def save_file(DF_file, foldername,file_key, folder_key,file_name=""):
     file_dict   = {0:"estate",1:'presale',2:'rent'}
     folder_dict = {0:"old",1:"new"}  # 新資料與舊資料
     filetype =  file_dict[file_key]+'_'+ folder_dict[folder_key] +'_'
-    save_time = dt.datetime.today().strftime('%Y%m') #儲存時間
-    file0 = file_name + filetype + save_time + r'.pickle' #儲存格是
+    save_time = dt.datetime.today().strftime('%Y%m_%d') #儲存時間
+    file0 =  filetype + save_time + r'.pickle' #儲存格式
     filename = Path(foldername) /file0
     with open(filename, 'wb') as ff:
         pickle.dump(DF_file, ff,protocol=pickle.HIGHEST_PROTOCOL)
     print('檔案於{a}儲存於{b}, 檔案名稱為~~{c}~~'.format(a=save_time, b=foldername, c=file0))
     
 
-def load_pickle(dir, key_id):
-    DF = pd.DataFrame()
-    key_file = {0:'estate', 1:'presale', 2:'rent'}
-    pickle_files = [jj for jj in os.listdir(dir) if re.search(key_file[key_id],jj)]
-    #print(pickle_files)
-    if isinstance(pickle_files, str):
-        pickle_files = [pickle_files] 
-    if not pickle_files ==[]: 
-        for jj in pickle_files: #可針對不同的pickle進行清理
-            file_dir = dir+'/'+ jj
-            with open(file_dir, 'rb') as ff:
-                result = pickle.load(ff)
-                DF = pd.concat([result, DF], axis=0, join='outer')
+
+if __name__ =='__main__':
+    variables = sys.argv
+    root1 = variables[1]
+    key_id = int(variables[2]) # 0: 成屋, 1:預售屋, 2:租賃
+    DF_file,B= collect_data(key_id,root1)
+    if len(variables) < 3:
+          print('Not enough parameters...')
+    elif len(variables) == 3:
+         save_bool = 0
     else:
-        result = 'The file empty!'
-        print(result)
-        DF = None
-    return DF
- 
+         save_bool = variables[3]
+
+    if save_bool:
+        save_loc0 = input('請輸入儲存位置...(若不輸入將直接存入原位置):\n') or root1 
+        folder_type = input('是否為季資料?[Y/N]')
+        folder_type = folder_type.upper()
+        folder_dict = {'Y':0, 'N':1}
+        save_file(DF_file, save_loc0,key_id, folder_dict[folder_type])
+    else:
+        print('不儲存,直接取消...')
+
+
+
+#save_file(DF_file, save_loc0,file_key, folder_key,file_name="")            
+# def load_pickle(dir, key_id):
+#     DF = pd.DataFrame()
+#     key_file = {0:'estate', 1:'presale', 2:'rent'}
+#     pickle_files = [jj for jj in os.listdir(dir) if re.search(key_file[key_id],jj)]
+#     #print(pickle_files)
+#     if isinstance(pickle_files, str):
+#         pickle_files = [pickle_files] 
+#     if not pickle_files ==[]: 
+#         for jj in pickle_files: #可針對不同的pickle進行清理
+#             file_dir = dir+'/'+ jj
+#             with open(file_dir, 'rb') as ff:
+#                 result = pickle.load(ff)
+#                 DF = pd.concat([result, DF], axis=0, join='outer')
+#     else:
+#         result = 'The file empty!'
+#         print(result)
+#         DF = None
+#     return DF
+
+
